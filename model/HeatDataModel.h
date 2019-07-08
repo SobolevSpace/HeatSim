67196634e95f608feb9ca115bf42489174e23a5a
qiaojx123 <631662546@qq.com>
2019-07-08 09:57:38 +0800

    Add HeatDataModel and HeatViewModel

---
+++ model/HeatDataModel.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "../others/ETL/include/etlbase.h"
+
+
+
+class HeatDataModel : public Proxy_PropertyNotification<HeatDataModel>
+{
+public:
+	HeatDataModel();
+
+
+private:
+
+};
