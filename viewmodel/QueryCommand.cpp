96dfec7cbf36c04c09f4c6abe04a17ff829166fe
qiaojx123 <631662546@qq.com>
2019-07-08 15:12:55 +0800

    Modified QueryCommand, HeatDataModel and HeatViewModel. Renamed QuertCommand.cpp for it's wrong.

---
+++ viewmodel/QueryCommand.cpp
@@ -0,0 +1,19 @@
+#include "QueryCommand.h"
+
+#include "HeatViewModel.h"
+
+QueryCommand::QueryCommand(HeatViewModel* p) throw() : m_pVM(p);
+{
+
+}
+
+void QueryCommand::SetParameter(const std::any& param)
+{
+	m_param = std::any_cast<workboard>(param);
+}
+
+void QueryCommand::Exec()
+{
+	bool bresult = m_pVM->Query(m_param);
+	m_pVM->Fire_OnCommandComplete("query", bresult);
+}
--- viewmodel/QueryCommand.h
+++ viewmodel/QueryCommand.h
@@ -4,3 +4,18 @@
 
 #include "../common/workboard.h"
 
+class HeatViewModel;
+
+class QueryCommand : public ICommandBase
+{
+public:
+	QueryCommand(HeatViewModel *p) throw();
+
+	virtual void SetParameter(const std::any& param);
+	virtual void Exec();
+
+private:
+	HeatViewModel* m_pVM;
+
+	workboard m_param;
+};
