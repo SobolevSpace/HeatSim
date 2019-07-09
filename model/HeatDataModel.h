daf94c9d76082ed556964f32ff57b78e6120ac0c
qiaojx123 <631662546@qq.com>
2019-07-09 09:56:37 +0800

    Finished HeatDataModel.

--- model/HeatDataModel.h
+++ model/HeatDataModel.h
@@ -10,12 +10,15 @@ public:
 	HeatDataModel();
 
 	void setInitCondi(const workboard condi);
+	void setInitCondi(const std::shared_ptr<workboard> condi);
 	std::shared_ptr<workboard> getInitCondi();
 	void setPadTemperature(const double temp);
 	double getPadTemperature() const;
 	void setAlpha(const double alp);
 	double getAlpha() const;
 	void setTime20(const int time);
+	void setTime20(const timeParameters);
+	void setTime20(const std::shared_ptr<timeParameters>);
 	int getTime20() const;
 	
 	std::shared_ptr<workboard> getResult();
