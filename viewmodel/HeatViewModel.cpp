b828e4ebe30c42fa2d9afb6212b8fb198a6539f0
qiaojx123 <631662546@qq.com>
2019-07-09 14:08:47 +0800

    Working on HeatViewModel

--- viewmodel/HeatViewModel.cpp
+++ viewmodel/HeatViewModel.cpp
@@ -1,1 +1,36 @@
 #include"HeatViewModel.h"
+
+HeatViewModel::HeatViewModel() : m_workboard(std::make_shared<workboard>())
+	m_cmdQuery(std::make_shared<QueryCommand>(this))
+	m_sink(std::make_shared<HeatViewModelSink>(this))
+{
+
+}
+
+void HeatViewModel::SetModel(const std::shared_ptr<HeatDataModel>& model)
+{
+	m_HeatModel = model;
+	m_HeatModel->AddPropertyNotification(std::static_pointer_cast<IPropertyNotification>(m_sink));
+}
+
+std::shared_ptr<workboard> HeatViewModel::getResult() throw()
+{
+	return m_workboard;
+}
+
+std::shared_ptr<ICommandBase> HeatViewModel::getQueryCommand()
+{
+	return std::static_pointer_cast<ICommandBase>(m_cmdQuery);
+}
+
+HeatDataModel& HeatViewModel::GetModel() throw()
+{
+	return *m_HeatModel;
+}
+
+void HeatViewModel::SetWorkboard(std::shared_ptr<workboard> board)
+{
+
+}
+
+bool Query();
--- viewmodel/HeatViewModel.h
+++ viewmodel/HeatViewModel.h
@@ -1,18 +1,31 @@
 #pragma once
 #include "../others/ETL/include/etlbase.h"
 #include "../model/HeatDataModel.h"
+#include "HeatViewModelSink.h"
 
 class HeatViewModel:public Proxy_PropertyNotification
 	:public Proxy_CommandNotification
 {
 public:
-	
+	HeatViewModel();
 
+	void SetModel(const std::shared_ptr<HeatDataModel>& model);
 
+	std::shared_ptr<workboard> getResult() throw();
+
+	std::shared_ptr<ICommandBase> getQueryCommand();
+
+	HeatDataModel& GetModel() throw();
 
 	void SetWorkboard(std::shared_ptr<workboard> board);
-
+	bool Query();
 
 private:
+	std::shared_ptr<HeatDataModel> m_HeatModel;
+
 	std::shared_ptr<workboard> m_workboard;
+
+	std::shared_ptr<QueryCommand> m_cmdQuery;
+
+	std::shared_ptr<HeatViewModelSink> m_sink;
 }
